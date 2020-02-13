e3778a0d840205d9529771b8bdca56b9abfffe9f
kqarlos <kqarlos@hotmail.com>
2020-02-12 21:04:49 -0800

    Adds global state

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,77 @@
+import React, { createContext, useReducer, useContext } from "react";
+import {
+    UPDATE_ITEMS,
+    REMOVE_CART_ITEM,
+    ADD_CART_ITEM,
+    SET_CURRENT_ITEM,
+    LOGIN,
+    LOGOUT
+} from "./actions"
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case UPDATE_ITEMS:
+            return {
+                ...state,
+                searchItems: [...action.searchItems],
+            };
+        case REMOVE_CART_ITEM:
+            return {
+                ...state,
+                cart: state.cart.filter((cart) => {
+                    return cart._id !== action._id;
+                })
+            };
+        case ADD_CART_ITEM:
+            return {
+                ...state,
+                cart: [action.item, ...state.cart]
+            };
+        case SET_CURRENT_ITEM:
+            return {
+                ...state,
+                currentItem: action.item,
+                loading: false
+            };
+        case LOGIN:
+            return {
+                ...state,
+                loggedin: true,
+                user: action._id
+            };
+        case LOGOUT:
+            return {
+                ...state,
+                loggedin: true,
+                user: ""
+            };
+        default:
+            return state;
+    }
+};
+
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useReducer(reducer, {
+        searchItems: [],
+        currentItem: {
+            _id: 0,
+            title: "",
+            price: "",
+            description: ""
+        },
+        cart: [],
+        loggedin: false,
+        userId: ""
+    });
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
---
+++ client/src/utils/actions.js
@@ -0,0 +1,6 @@
+export const UPDATE_ITEMS = "UPDATE_CART";
+export const REMOVE_CART_ITEM = "REMOVE_CART_ITEM";
+export const ADD_CART_ITEM = "ADD_CART_ITEM";
+export const SET_CURRENT_ITEM = "SET_CURRENT_ITEM";
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
