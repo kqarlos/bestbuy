3977bd8a974db167fafb4dcbdc9446b8a0f3684c
kqarlos <kqarlos@hotmail.com>
2022-06-20 18:36:39 -0700

    updating products request to API

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -7,19 +7,17 @@ module.exports = {
     if (!product) {
       product = "iphone";
     }
-    axios
-      .get(
-        `https://api.bestbuy.com/v1/products(longDescription=${
-          product
-        }*)?format=json&apiKey=${process.env.BEST_BUY_API_KEY}`
-      )
-      .then(results => {
-        // console.log("RESULTS: ", results.data);
-        res.json([...results.data.products]);
+
+    let productsAPIURL = `https://api.bestbuy.com/v1/products(longDescription=${product
+      }*)?format=json&apiKey=${process.env.BEST_BUY_API_KEY}`;
+
+    axios.get(productsAPIURL)
+      .then(({ data: { products } }) => {
+        res.json([...products]);
       })
       .catch(err => console.log(err));
   },
-  findByClass: function(req, res) {
+  findByClass: function (req, res) {
     if (req.query.q === "") {
       req.query.q = "mobile";
     }
@@ -36,29 +34,27 @@ module.exports = {
     //   })
     //   .catch(err => console.log(err));
   },
-  findById: function(req, res) {
-    axios
-      .get(
-        `https://api.bestbuy.com/v1/products(productId=${
-          req.params.id
-        })?format=json&apiKey=${process.env.BEST_BUY_API_KEY}`
-      )
-      .then(results => {
-        res.json(results.data.products[0]);
+  findById: function (req, res) {
+    let findIDURL = `https://api.bestbuy.com/v1/products(productId=${req.params.id
+      })?format=json&apiKey=${process.env.BEST_BUY_API_KEY}`;
+
+    axios.get(findIDURL)
+      .then(({ data: { products } }) => {
+        res.json(products[0]);
       })
       .catch(err => console.log(err));
   },
-  create: function(req, res) {
+  create: function (req, res) {
     db.Product.create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  update: function(req, res) {
+  update: function (req, res) {
     db.Product.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  remove: function(req, res) {
+  remove: function (req, res) {
     db.Product.findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
